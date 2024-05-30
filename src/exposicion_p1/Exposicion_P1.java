3916284559bd2c70d1b66e76da427189c3d748a8
Eliasegg <elias-eeg@hotmail.com>
2024-05-30 07:47:17 -0600

    Primer commit

---
+++ src/exposicion_p1/Exposicion_P1.java
@@ -0,0 +1,16 @@
+package exposicion_p1;
+
+/**
+ *
+ * @author Yuli y Elias
+ */
+public class Exposicion_P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
